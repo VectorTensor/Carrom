0c1b86691faf8cc9c8f64786880a9f3aec3ae9f1
RamanMhzn123 <ramanmaharjan03@gmail.com>
2023-06-01 12:27:57 +0545

    Added slider for striker and reset position function

--- Assets/Scripts_R/Striker_R.cs
+++ Assets/Scripts_R/Striker_R.cs
@@ -2,71 +2,28 @@ using UnityEngine;
 
 public class Striker_R : MonoBehaviour
 {
-    public float y;
     public float strikerForce;
-
+    public float magnitude;
     Rigidbody rb;
-    bool confirmPosition = false;
+    Vector3 startPos;
 
-    [SerializeField] GameObject confirmButton;
-    GameObject arrow ;
-
-    private void Start()
+    void Start()
     {
-        arrow = gameObject.transform.GetChild(0).gameObject;
         rb = GetComponent<Rigidbody>();
-    }
-    
-    void FixedUpdate()
-    {
-        if (!confirmPosition) //if button not clicked
-        {
-            if (Input.GetMouseButtonDown(1))
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-                    float hitX = Mathf.Clamp(hit.point.x, -1.3f, 1.3f);
-                    Vector3 newPostion = new Vector3(hitX, transform.position.y, transform.position.z);
-                    rb.MovePosition(newPostion);
-                }
-            }
-        }
-
-        //Set direction
-        y = arrow.transform.localRotation.eulerAngles.y;
-        transform.localEulerAngles = new Vector3(0, y, 0);
-
-        //Add force
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Debug.Log("Added Force");
-            rb.AddRelativeForce(Vector3.forward * strikerForce * ForceSlider.currentValue);
-        }
-    }
-
-    /*void OnEnable()
-    {
-        ForceSlider.onforceset += Attack;
+        startPos = transform.position;
     }
 
     void Attack()
     {
-        Debug.Log("Add force ");
-        Rigidbody rb = GetComponent<Rigidbody>();
-    }*/
-
-    public void ConfirmButton()
-    {
-        confirmPosition = true;
-        confirmButton.SetActive(false);
-        arrow.SetActive(!arrow.activeSelf);
+        //rb.AddForce(direction * force * forceMultipler);
+        Vector3 force = Vector3.forward * strikerForce * ForceSlider.currentValue;
+        rb.AddRelativeForce(force);
+        UIManager.hasStriked = true;
+        magnitude = rb.velocity.magnitude;
     }
 
-    /* IEnumerator ResetStrikerPos()
-     {
-         yield return new WaitForSeconds(2.0f);
-         transform.position = new Vector3(0, 0.1f, -1.7f);
-     }*/
+    void OnEnable()
+    {
+        ForceSlider.onforceset += Attack;
+    }
 }
