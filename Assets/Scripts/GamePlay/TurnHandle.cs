f4f72e875c3e49624c7ec929c1bbcb55078b89a5
RamanMhzn123 <ramanmaharjan03@gmail.com>
2023-06-02 09:03:51 +0545

    Fixed null reference expections

--- Assets/Scripts/GamePlay/TurnHandle.cs
+++ Assets/Scripts/GamePlay/TurnHandle.cs
@@ -1,57 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.Collections;
 
 public class TurnHandle : MonoBehaviour
 {
-    private int turn; 
-    private int total_numbers_of_players =2 ;
-    // Start is called before the first frame update
+    int turn; 
+    int total_numbers_of_players = 2 ;
+
+    public GameObject hidder;
+
     void Start()
     {
-        
         total_numbers_of_players = 2;
         turn = 1;
+
         //PhotonView photonView = PhotonView.Get(this);
         //photonView.RPC("helloEveryone",RpcTarget.All,"hello mate");
         Debug.Log(PhotonNetwork.LocalPlayer.ActorNumber) ;
-        
+
+        hidder.SetActive(true);
     }
-    [PunRPC]
-    void messageEveryone(string message){
-        Debug.Log(message);
-
-    }
-
-    [PunRPC]
-    void sendTurn(int turn){
-        this.turn = turn;
-    }
-    [PunRPC]
-    void nextTurn(){
-        // How next turn is calculated
-        turn = turn  % total_numbers_of_players+1; 
-
-        
-        PhotonView photonView = PhotonView.Get(this);
-        photonView.RPC("messageEveryone",RpcTarget.All,"Player turn "+ turn );
-        
-        photonView.RPC("sendTurn",RpcTarget.Others, turn );
-
-
-    }
-
-    // Update is called once per frame
+   
     void Update()
     {
         //&& turn == int.Parse(PhotonNetwork.LocalPlayer.ActorNumber)
+        //hasStrike = true
+        if (turn == PhotonNetwork.LocalPlayer.ActorNumber ) 
+        {
+            //enable UI
+            hidder.SetActive(false);
 
-        if (Input.GetMouseButtonDown(0) && turn == PhotonNetwork.LocalPlayer.ActorNumber ) {
+            //PhotonView photonView = PhotonView.Get(this);
+            //photonView.RPC("nextTurn",RpcTarget.MasterClient);
+        }
+        else
+        {
+            //enable UI
+            hidder.SetActive(true);
+        }
+    }
+
+    [PunRPC]
+    void nextTurn()
+    {
+        // How next turn is calculated
+        turn = turn  % total_numbers_of_players+1;
 
         PhotonView photonView = PhotonView.Get(this);
-        photonView.RPC("nextTurn",RpcTarget.MasterClient);
-        }
-        
+        photonView.RPC("messageEveryone", RpcTarget.All, "Player turn "+ turn);
+
+        photonView.RPC("sendTurn", RpcTarget.Others, turn);
+
+        StartCoroutine("SetTrue");
+    }
+
+    [PunRPC]
+    void messageEveryone(string message)
+    {
+        Debug.Log(message);
+    }
+
+    [PunRPC]
+    void sendTurn(int turn)
+    {
+        this.turn = turn;
+    }
+
+    IEnumerable SetTrue()
+    {
+        yield return new WaitForSeconds(2);
+        hidder.SetActive(true);
     }
 }
